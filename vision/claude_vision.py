a45faf1b76d8ec665a392bc984b84026ea06f13a
Bart Trzynadlowski <bart.trzy@gmail.com>
2024-03-29 21:38:05 -0700

    Fix JPEG detection logic

--- vision/claude_vision.py
+++ vision/claude_vision.py
@@ -65,7 +65,7 @@ class ClaudeVision(Vision):
     def _detect_media_type(image_bytes: bytes) -> str:
         if image_bytes[0:4] == b"\x89PNG":
             return "image/png"
-        elif image_bytes[0:64] == b"JFIF":  # probably should do a stricter check here
+        elif b"JFIF" in image_bytes[0:64]:  # probably should do a stricter check here
             return "image/jpeg"
         elif image_bytes[0:4] == b"RIFF" and image_bytes[8:12] == b"WEBP":
             return "image/webp"
