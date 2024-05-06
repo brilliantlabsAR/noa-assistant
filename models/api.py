f132023a79e6ab18bbd0331d48ca724ec7ad65ea
Bart Trzynadlowski <bart.trzy@gmail.com>
2024-05-06 00:28:28 -0700

    MultimodalRequest: add assistant parameter back

--- models/api.py
+++ models/api.py
@@ -59,7 +59,8 @@ class MultimodalRequest(BaseModel):
     messages: Optional[List[Message]]
     prompt: Optional[str] = ""
     noa_system_prompt: Optional[str] = None
-    assistant_model: Optional[str] = None
+    assistant: Optional[str] = None         # assistant class: gpt, claude, perplexity, groq
+    assistant_model: Optional[str] = None   # specific model for the assistant class
     search_api: Optional[SearchAPI] = SearchAPI.SERP
     search_engine: Optional[SearchEngine] = SearchEngine.GOOGLE
     max_search_results: Optional[int] = 10
