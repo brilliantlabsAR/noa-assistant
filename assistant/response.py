36e253c90ed1a098e611cc2fa5dcffd357ee5a39
Bart Trzynadlowski <trzy@users.noreply.github.com>
2024-06-18 23:37:11 -0700

    Remove _error_response() from AssistantResponse (no longer used)

--- assistant/response.py
+++ assistant/response.py
@@ -18,27 +18,3 @@ class AssistantResponse:
     response: str
     timings: Dict[str, float]
     image: str
-
-    @staticmethod
-    def _error_response(message: str) -> AssistantResponse:
-        """
-        Generates an error response intended as a tool output.
-
-        Parameters
-        ----------
-        message : str
-            Error message, intended to be consumed by LLM tool completion pass.
-
-        Returns
-        -------
-        AssistantResponse
-            AssistantResponse object.
-        """
-        return AssistantResponse(
-            token_usage_by_model={},    # TODO
-            capabilities_used=[],
-            response=message,
-            timings={},
-            image="",
-            stream_finished=True
-        )
